1881b6f1ec7c3c1814f45436bc4161dfbdc6586c
Rodrigo Santomauro <rodrigoolema@outlook.com>
2022-06-10 22:33:23 -0300

    actualizacion registry

--- terraform/registry.tf
+++ terraform/registry.tf
@@ -1,9 +1,13 @@
 # Se crea el repositorio donde se almacenaran las imagenes de los microservicios Boutique
-resource "aws_ecr_repository" "boutique_registry" {
-  name                 = "boutique"
+resource "aws_ecr_repository" "boutique_ecr" {
+  name                 = "boutique_ecr"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
+  }
+
+  tags = {
+    Name       = "boutique_ecr"
   }
 }
