3a02ba5c9ab3f07f99eb665e122bfb24f106d4cd
Rodrigo Santomauro <rodrigoolema@outlook.com>
2022-06-10 21:43:50 -0300

    Agrego SG

---
+++ terraform/security-groups.tf
@@ -0,0 +1,39 @@
+resource "aws_security_group" "boutique_eks_sg" {
+  name   = "boutique_eks_sg"
+  vpc_id = aws_vpc.boutique_vpc.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "boutique_eks_sg"
+  }
+}
+
+resource "aws_security_group" "boutique_lb_sg" {
+  name   = "boutique_lb_sg"
+  vpc_id = aws_vpc.boutique_vpc.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "boutique_lb_sg"
+  }
+}
